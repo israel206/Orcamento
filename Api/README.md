2e20a4cc38661b67a486b2d33fb9129f44588693
israel <israel.pereira@dcx.ufpb.br>
2021-05-26 12:32:40 -0300

    back-end api🧰🧰

---
+++ Api/README.md
@@ -0,0 +1,97 @@
+<h1 align="center">
+    <img alt="orcamento-back" title="orcamento-back" src="" />
+</h1>
+
+<p align="center">
+  <a href="#technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-license">License</a>
+</p>
+
+<p align="center">
+
+  [![Author](https://img.shields.io/badge/author-IsraelSilva-8257E5?style=flat-square)](https://github.com/israel206)
+  [![Languages](https://img.shields.io/github/languages/count/israel206/orcamento-back?color=%238257E5&style=flat-square)](#)
+  <img src="https://img.shields.io/github/forks/israel206/orcamento-back?label=forks&message=MIT&color=FFFFFF&labelColor=32B768" alt="Forks">    
+  <img src="https://img.shields.io/github/stars/israel206/orcamento-back?label=stars&message=MIT&color=FFFFFF&labelColor=32B768" alt="Stars">
+  <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=49AA26&labelColor=000000" alt="PRs welcome!" />
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+---
+
+## 🚀 Technologies
+
+This project was developed using the following technologies:
+
+  - [Node](https://nodejs.org/en/download/)
+
+---
+
+## 👨‍💻 Computer: How to run
+
+**Clone the project and access the folder**
+  ```bash
+    # clone repository
+    $ git clone https://github.com/israel206/orcamento-back.git
+  ```
+---
+
+## 🚀 Getting started
+
+### Requirements
+
+- You need to install both [Node.js](https://nodejs.org/en/download/) and [Yarn](https://yarnpkg.com/) to run this project.
+
+**Follow the steps below**
+
+`HOW TO RUN THE DOWNLOADED PROJECT
+Install all dependencies indicated by package.json
+### npm install
+
+Run the project using the nodemon
+### nodemon app.js
+
+
+SEQUENCE TO CREATE THE PROJECT
+Create the package file
+### npm init
+
+Manages requests, routes and URLs, among other features
+### npm install express
+
+Install the module to restart the server whenever there is a change in the source code, g means globally
+### npm install -g nodemon
+
+Install the MongoDB database
+### npm install --save mongodb
+
+Install Mongoose - Mongoose translates the database data into JavaScript objects so that they can be used by the application.
+### npm install --save mongoose
+
+Allow API access
+### npm install --save cors
+
+---
+
+## 💻 Project
+
+App to always remember to water your plants 🌱
+
+API under development
+
+---
+
+## 📝 License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE.md) file for details.
+
+---
+
+Made with 💜 by Israel Silva 👋
+
+[ts]: https://www.typescriptlang.org
+[vscode]: https://code.visualstudio.com/
+[yarn]: https://yarnpkg.com/
+[vceditconfig]: https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+[vceslint]: https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
---
+++ Api/app.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('./models/home');
+const Home = mongoose.model('Home');
+
+require('./models/orcamento');
+const Orcamento = mongoose.model('Orcamento');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+});
+
+mongoose.connect('mongodb://localhost:27017/celke', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Conexão com o BD MongoDB realizado com sucesso!");
+}).catch((err) => {
+    console.log("Erro: Conexão com o BD MongoDB não realizado com sucesso!");
+})
+
+
+app.get('/home', async (req, res) => {
+    await Home.findOne({}).then((home) => {
+        return res.json({
+            error: false,
+            home
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum registro encontrado!"
+        });
+    });
+});
+
+app.post('/home', async (req, res) => {
+    const dados = {
+        "topTitulo": "Temos a solução que a sua empresa precisa!",
+        "topSubtitulo": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "topTextoBtn": "Orçamento",
+        "topLinkBtn": "http://localhost:3000/orcamento",
+
+        "serTitulo": "Serviços",
+        "serSubtitulo": "Featured content or information",
+        "serUmIcone": "laptop-code",
+        "serUmTitulo": "Serviço um",
+        "serUmDesc": "Praesent quis sagittis libero, nec suscipit neque. Quisque ut ultrices lectus, sit amet sollicitudin mauris.",
+        "serDoisIcone": "mobile-alt",
+        "serDoisTitulo": "Serviço dois",
+        "serDoisDesc": "Nullam rutrum imperdiet nisi, eget facilisis elit consectetur accumsan lectus, sit amet sollicdin efficitur.",
+        "serTresIcone": "network-wired",
+        "serTresTitulo": "Serviço tres",
+        "serTresDesc": "Quisque elementum suscipit dolor, sed lobortis nibh. Curabitur et dui iaculis, consectetur enim vitae purus.",
+    }
+
+    const homeExiste = await Home.findOne({});
+    if(homeExiste){
+        return res.status(400).json({
+            error: true,
+            message: "Erro: A página home já possui um registro!"
+        });
+    }
+
+    await Home.create(dados, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro: Conteúdo da página home não cadastrado com sucesso!"
+        });
+    });
+
+    return res.json({
+        error: false,
+        message: "Conteúdo da página home cadastrado com sucesso!"
+    });
+});
+
+app.post('/orcamento', async (req, res) => {    
+
+    await Orcamento.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro: Solicitação de orçamento enviado com sucesso!"
+        });
+    });
+
+    return res.json({
+        error: false,
+        message: "Solicitação de orçamento enviado com sucesso!"
+    });
+});
+
+app.listen(8080, function () {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
